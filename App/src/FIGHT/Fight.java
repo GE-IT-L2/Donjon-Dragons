1b602d18a71c33196d3b3d649dedb74afe7d6452
Kamariaaa <andrianjafyzaraina@gmail.com>
2026-02-26 11:05:17 +0300

    modification fight.java

--- App/src/FIGHT/Fight.java
+++ App/src/FIGHT/Fight.java
@@ -1,5 +1,38 @@
 package FIGHT;
+import MODE_1.Character;
 
 public class Fight {
-    
+    private Character character1;
+    private Character character2;
+
+    public Fight(Character character1, Character character2){
+        this.character1 = character1;
+        this.character2 = character2;
+    }
+
+    public void startFight(){
+        while(character1.isAlive() && character2.isAlive()){
+            if(character1.getSpeed() >= character2.getSpeed()){
+                character1.attack(character2);
+                if(character2.isAlive()){
+                    character2.attack(character1);
+                }
+            } else {
+                character2.attack(character1);
+                if(character1.isAlive()){
+                    character1.attack(character2);
+                }
+            }
+        }
+    }
+
+    public void displayWinner(){
+        if(character1.isAlive()){
+            System.out.println("Character 1 wins!");
+        } else if(character2.isAlive()){
+            System.out.println("Character 2 wins!");
+        } else {
+            System.out.println("It's a draw!");
+        }
+    }
 }
