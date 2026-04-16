adf50e195cce12a7709b30bc324fd392dfd9c895
Kamariaaa <andrianjafyzaraina@gmail.com>
2026-04-16 09:23:35 +0300

    modification App.java

--- App/src/App.java
+++ App/src/App.java
@@ -8,10 +8,8 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("==============================================================");
         System.out.println("         DONJON & DRAGON - THE CURSED DUNGEON (MODE 1)     ");
         System.out.println("            Strategic Turn-Based Java Edition                  ");
-        System.out.println("==============================================================");
 
         // Create the party (you can change or add more heroes here)
         List<Character> party = new ArrayList<>();
